70febd4f4496a0de3c0e09ed26be60e28809f6e0
Diogo Magalhães <d.magalhaes@ua.pt>
2025-01-27 12:18:40 +0000

    improvements to one-click deployment

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
-helm -n meao uninstall osm-mec
-docker build -t localhost:5000/cfs-portal cfs-portal
-docker push localhost:5000/cfs-portal
-docker build -t localhost:5000/meao meao
-docker push localhost:5000/meao
-docker build -t localhost:5000/oss oss
-docker push localhost:5000/oss
-helm -n meao upgrade --install osm-mec osm-mec-helm-chart
+helm -n osm-mec uninstall osm-mec
+
+docker build -t localhost:5000/cfs-portal:latest cfs-portal
+docker push localhost:5000/cfs-portal:latest
+docker build -t localhost:5000/meao:latest meao
+docker push localhost:5000/meao:latest
+docker build -t localhost:5000/oss:latest oss
+docker push localhost:5000/oss:latest
+
+OSM_NBI=$(kubectl -n osm get ingress nbi-ingress -o jsonpath='{.spec.rules[0].host}')
+[ -z "$K8S_DEFAULT_IF" ] && K8S_DEFAULT_IF=$(ip route list|awk '$1=="default" {print $5; exit}')
+[ -z "$K8S_DEFAULT_IF" ] && K8S_DEFAULT_IF=$(route -n |awk '$1~/^0.0.0.0/ {print $8; exit}')
+[ -z "$K8S_DEFAULT_IF" ] && FATAL "Not possible to determine the interface with the default route 0.0.0.0"
+K8S_DEFAULT_IP=`ip -o -4 a s ${K8S_DEFAULT_IF} |awk '{split($4,a,"/"); print a[1]; exit}'`
+
+helm -n osm-mec upgrade --install osm-mec osm-mec-helm-chart --set cfsPortal.deployment.image=localhost:5000/cfs-portal:latest --set meao.deployment.image=localhost:5000/meao:latest --set oss.deployment.image=localhost:5000/oss:latest --set osm.host=$OSM_NBI --set cfsPortal.ossHost=$K8S_DEFAULT_IP
 kubectl get pods -A
