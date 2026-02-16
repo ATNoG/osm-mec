e16027d98ea5faa436b110edce095852f0996dd1
oEscal <pedroescaleira@hotmail.com>
2026-02-16 20:17:36 +0000

    metrics forwarder computing available cpu and memory in the same way as the meao monitoring
    Signed-off-by: oEscal <pedroescaleira@hotmail.com>

--- metrics-forwarder/src/callbacks/meh-metrics.py
+++ metrics-forwarder/src/callbacks/meh-metrics.py
@@ -1,5 +1,4 @@
 from src.utils.exceptions import handle_exceptions
-import re
 import logging
 from src.utils.kafka.kafka_utils import KafkaUtils
 
@@ -18,8 +17,9 @@ def callback(forwarder, message):
         # Update the nodes with the available CPU and memory
         for cluster in message["nodeSpecs"]:
             for node, specs in message["nodeSpecs"][cluster]["nodeSpecs"].items():
-                available_cpu = specs["num_cpu_cores"] - specs["allocated-cpu"] - (specs.get("cpuUsage", 0) - specs.get("appis-cpuUsage", 0))
-                available_mem = specs["memory_size"] - specs["allocated-mem"] - (specs.get("memUsage", 0) - specs.get("appis-memUsage", 0))
+                available_cpu = max(0, specs["num_cpu_cores"] - max(specs.get("appis-cpuUsage", 0), specs.get("allocated-cpu", 0), specs.get("cpuUsage", 0)))
+                available_mem = max(0, specs["memory_size"] - max(specs.get("appis-memUsage", 0), specs.get("allocated-mem", 0), specs.get("memUsage", 0)))
+                # logging.info(f"""{available_mem, specs["memory_size"], max(specs.get("appis-memUsage", 0), specs.get("allocated-mem", 0), specs.get("memUsage", 0))}""")
                 message["nodeSpecs"][cluster]["nodeSpecs"][node]["available-cpu"] = available_cpu
                 message["nodeSpecs"][cluster]["nodeSpecs"][node]["available-mem"] = available_mem
 
